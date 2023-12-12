21047f67f3b9bcdd61ab28d3a3f448a4c8d595e2
SBAPKat <50132527+SBAPKat@users.noreply.github.com>
2023-12-12 17:26:35 +0100

    Initial commit

---
+++ Core/Inc/MCP3008.h
@@ -0,0 +1,37 @@
+/*
+ * MCP3008.h
+ *
+ *  Created on: Dec 5, 2023
+ *      Author: sbapk
+ */
+
+#ifndef INC_MCP3008_H_
+#define INC_MCP3008_H_
+#include "stm32f4xx_hal.h"
+
+#define ADC_START 0b10000000
+#define ADC_SINGLE_ENDED_MEASURE_MODE 0b01000000
+
+typedef enum
+{
+  ADC_OK       = 0x00U,
+  ADC_XFER_ERROR     = 0x01U,
+  ADC_TIMEOUT  = 0x02U,
+  ADC_CHANN_ID_OOB = 0x04U
+} ADC_StatusTypeDef;
+
+typedef struct{
+	SPI_HandleTypeDef* phspi;
+	GPIO_TypeDef* pgpio_cs;
+	uint16_t pin_nbr;
+	ADC_StatusTypeDef status;
+	float vref;
+}MCP3008_InitTypeDef;
+
+
+HAL_StatusTypeDef MCP3008_ReadChannel(MCP3008_InitTypeDef* MCP3008_dev, uint8_t channel_id, float* result, uint32_t timeout);
+HAL_StatusTypeDef MCP3008_ReadAllChannels(MCP3008_InitTypeDef* MCP3008_dev, float* result, uint32_t timeout);
+HAL_StatusTypeDef MCP3008_Polling_Benchmark(MCP3008_InitTypeDef* MCP3008_dev, uint8_t channel_id, float* ksps, uint32_t timeout);
+
+
+#endif /* INC_MCP3008_H_ */
---
+++ Core/Src/MCP3008.c
@@ -0,0 +1,70 @@
+/*
+ * MCP3008_dev.c
+ *
+ *  Created on: Dec 5, 2023
+ *      Author: sbapk
+ */
+#include "MCP3008.h"
+
+HAL_StatusTypeDef MCP3008_ReadChannel(MCP3008_InitTypeDef* MCP3008_dev, uint8_t channel_id, float* result, uint32_t timeout){
+	uint8_t TxData[3] = {0};
+	uint8_t RxData[3] = {0};
+	uint16_t RxData_16b = 0;
+	MCP3008_dev->status = ADC_OK;
+
+	if(channel_id > 7){
+		/* Channel id out of bounds */
+		MCP3008_dev->status |= ADC_CHANN_ID_OOB;
+		return HAL_ERROR;
+	}
+
+	/* Building data to be sent, start bit + measure mode + channel ID */
+	TxData[0] |= ADC_START | ADC_SINGLE_ENDED_MEASURE_MODE | ((channel_id& 0x07)<<3);
+
+	/* Select chip */
+	HAL_GPIO_WritePin(MCP3008_dev->pgpio_cs, MCP3008_dev->pin_nbr, GPIO_PIN_RESET);
+
+
+	/* Start transfer */
+	if( HAL_SPI_TransmitReceive(MCP3008_dev->phspi, TxData, RxData, 3, timeout) != HAL_OK){
+		MCP3008_dev->status |= ADC_XFER_ERROR;
+
+	}
+
+	/* Un-select Chip */
+	HAL_GPIO_WritePin(MCP3008_dev->pgpio_cs, MCP3008_dev->pin_nbr, GPIO_PIN_SET);
+
+
+	/* Translate the data to a 16bit value */
+	RxData_16b = 0x3FF & ((RxData[0] & 0x01) << 9 | (RxData[1] & 0xFF) << 1 | (RxData[2] & 0x80) >> 7);
+
+	/* Get the real value */
+	*result = RxData_16b * MCP3008_dev->vref /1024;
+
+
+	if(MCP3008_dev->status != ADC_OK) return HAL_ERROR;
+	return HAL_OK;
+
+
+}
+
+
+HAL_StatusTypeDef MCP3008_ReadAllChannels(MCP3008_InitTypeDef* MCP3008_dev, float* result, uint32_t timeout){
+	for( uint8_t channel = 0 ; channel < 8 ; channel++ ){
+		if( MCP3008_ReadChannel(MCP3008_dev, channel, result, timeout) != HAL_OK ) return HAL_ERROR;
+		result++;
+	}
+	return HAL_OK;
+}
+
+
+HAL_StatusTypeDef MCP3008_Polling_Benchmark(MCP3008_InitTypeDef* MCP3008_dev, uint8_t channel_id, float* ksps, uint32_t timeout){
+	uint32_t conversion_number = 0;
+	uint32_t start = HAL_GetTick();
+	while( HAL_GetTick() - start < 1000){
+		if( MCP3008_ReadChannel(MCP3008_dev, channel_id, NULL, timeout) != HAL_OK) return HAL_ERROR;
+		conversion_number++;
+	}
+	*ksps = ( conversion_number / 1000.0 ) / ( timeout / 1000.0 );
+	return HAL_OK;
+}
